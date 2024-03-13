1ed5579e43107c7a460bf6cd26c72fd2e84c7db2
DarKRs <centralstreet_24@mail.ru>
2024-03-13 15:32:19 +0600

    Очистка

--- TaskWave.Domain/Entities/ProjectTask.cs
+++ TaskWave.Domain/Entities/ProjectTask.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace TaskWave.Domain.Entities
 {
-	public class ProjectTask
+    public class ProjectTask
     {
         [Key]
         public int TaskId { get; set; }
-		public string Description { get; set; }
-		public int ProjectId { get; set; }
-		public Project Project { get; set; }
-	}
+        public string Description { get; set; }
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+    }
 
 }
--- TaskWave.Infrastructure/Data/TaskManagementContext.cs
+++ TaskWave.Infrastructure/Data/TaskManagementContext.cs
@@ -1,8 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using TaskWave.Domain.Entities;
 
 namespace TaskWave.Infrastructure.Data
