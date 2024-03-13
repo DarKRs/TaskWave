93f67574996dabffa6379422d91427e8dfc26148
DarKRs <centralstreet_24@mail.ru>
2024-03-13 21:37:07 +0600

    HomeController

---
+++ TaskWave.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskWave.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
